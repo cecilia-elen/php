3b2d10ea13ae0def77ef69751ba05cf63bf763c4
Cecília <145465067+cecilia-elen@users.noreply.github.com>
2024-05-07 11:53:27 -0300

    Update exercicio.php

--- aula-07-05/exercicio.php
+++ aula-07-05/exercicio.php
@@ -1,6 +1,7 @@
 <?php
 
-$alunos = array("kailla", "mariana", "bruna", "diamilli", "maria antonia", "raifran", "ivan", "gustavo", "sarah");
+$alunos = array("kailla", "mariana", "bruna", "diamilli",
+ "maria antonia", "raifran", "ivan", "gustavo", "sarah");
 
 echo "exemplo com for <br>";
 for($i=0; $i<(count($alunos));$i++){
@@ -9,9 +10,11 @@ for($i=0; $i<(count($alunos));$i++){
         echo "$alunos[$i]<br>";
 
     }
-
+ }
     echo "exemplo com foreach <br>";
     foreach($alunos as $dados){
-        echo $dados;
-    }
+        echo "$dados <br>";
+   
 }
+
+?>
