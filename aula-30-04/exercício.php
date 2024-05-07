a5c7f7bd216136dec53cdaeeddd0ec5afaa7bc82
Cecília <145465067+cecilia-elen@users.noreply.github.com>
2024-05-07 08:21:42 -0300

    Update exercício.php

--- aula-30-04/exercício.php
+++ aula-30-04/exercício.php
@@ -1,5 +1,6 @@
 <?php
 
+echo "<br>Aluno 1: <br>";
 $aluno1["nome"] = "Cecília";
 $aluno1["idade"] = 18;
 $aluno1["endereço"] = "Minha casa";
@@ -7,12 +8,14 @@ $aluno1["cidade"] = "Guanambi";
 $aluno1["estado"] = "Bahia";
 $aluno1["cpf"] = "40028922";
 
+echo "Nome:" . $aluno1["nome"]; 
+echo "<br>Idade:" . $aluno1["idade"];
+echo "<br>Endereço:" . $aluno1["endereço"]; 
+echo "<br>Cidade:" . $aluno1["cidade"];
+echo "<br>Estado:" . $aluno1["estado"];
+echo "<br>CPF:" . $aluno1["cpf"];         
 
-echo "Aluno 1: <br>";
-foreach ($aluno1 as $a) {
-    echo  key($aluno1) . ": $a  <br>";
-    next($aluno1);
-    }
+
     
 $aluno2["nome"] = "Raifran";
 $aluno2["idade"] = 18;
@@ -22,11 +25,12 @@ $aluno2["estado"] = "Bahia";
 $aluno2["cpf"] = "777";
 
 echo "<br>Aluno 2: <br>";
-foreach ($aluno2 as $c) {
-    echo  key($aluno2) . ": $c  <br>";
-    next($aluno2);
-}
-    
+echo "<br>Nome:" . $aluno2["nome"]; 
+echo "<br>Idade:" . $aluno2["idade"];
+echo "<br>Endereço:" . $aluno2["endereço"]; 
+echo "<br>Cidade:" . $aluno2["cidade"];
+echo "<br>Estado:" . $aluno2["estado"];
+echo "<br>CPF:" . $aluno2["cpf"];  
     
 $professor["nome"] = "Fábio";
 $professor["idade"] = 30;
@@ -36,8 +40,11 @@ $professor["estado"] = "Bahia";
 $professor["cpf"] = "000000000";
 
 echo "<br>Professor: <br>";
-foreach ($professor as $d) {
-    echo  key($professor) . ": $d  <br>";
-    next($professor);
-} 
+echo "<br>Nome:" . $professor["nome"]; 
+echo "<br>Idade:" . $professor["idade"];
+echo "<br>Endereço:" . $professor["endereço"]; 
+echo "<br>Cidade:" . $professor["cidade"];
+echo "<br>Estado:" . $professor["estado"];
+echo "<br>CPF:" . $professor["cpf"];  
+
 ?>
